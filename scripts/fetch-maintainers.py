c05d26dec5b46cc00c0452226555119ff7056651
phanirithvij <phanirithvij2000@gmail.com>
2026-07-18 11:12:29 +0530

    style: add treefmt and format codebase

--- scripts/fetch-maintainers.py
+++ scripts/fetch-maintainers.py
@@ -2,7 +2,6 @@
 #!nix-shell -i python3 -p python3 python3.pkgs.multiprocess python3.pkgs.gitpython
 
 
-
 import os
 import git
 from multiprocessing import Pool, Manager
@@ -26,7 +25,9 @@ def clone_nixpkgs(rev, nixos):
     origin.fetch(refspec=rev)
     repo.git.reset(rev, hard=True)
     if nixos:
-        print("Applying do_not_remove_maintainers.patch to nixos/release-combined.nix...")
+        print(
+            "Applying do_not_remove_maintainers.patch to nixos/release-combined.nix..."
+        )
         repo.git.apply(f"{owd}/scripts/do_not_remove_maintainers.patch")
     os.chdir(owd)
 
@@ -42,15 +43,19 @@ def find_maintainer_for_job(job_name, nixos, res, job_maintainers):
         file_to_evaluate = "./data/nixpkgs/pkgs/top-level/release.nix"
     try:
         if name_without_arch not in job_maintainers.keys():
-           r = ast.literal_eval(subprocess.check_output(f"nix eval --json -f {file_to_evaluate} {real_job_name}.meta.maintainers 2> /dev/null", shell=True).decode("utf-8"))
-           job_maintainers[name_without_arch] = r
-           res[job_name] = r
+            r = ast.literal_eval(
+                subprocess.check_output(
+                    f"nix eval --json -f {file_to_evaluate} {real_job_name}.meta.maintainers 2> /dev/null",
+                    shell=True,
+                ).decode("utf-8")
+            )
+            job_maintainers[name_without_arch] = r
+            res[job_name] = r
         else:
-           res[job_name] = job_maintainers[name_without_arch]
+            res[job_name] = job_maintainers[name_without_arch]
 
     except Exception as _:
         res[job_name] = ["error"]
-
 
 
 def main(evals):
@@ -64,7 +69,7 @@ def main(evals):
             jobs = []
             jobs_info = {}
             for line in f.readlines():
-                status  = line.split(" ")
+                status = line.split(" ")
                 if "failed" in status[-1].strip().lower():
                     job_name = status[0].strip()
                     if not ev[2]:
@@ -75,7 +80,7 @@ def main(evals):
                 p.starmap(find_maintainer_for_job, jobs)
 
             f = open(f"data/maintainerscache/{ev[0]}.cache", "a")
-            for (k, v) in res.items():
+            for k, v in res.items():
                 if v == []:
                     f.write(f"_ {k} {' '.join(jobs_info[k])}")
                 for maint in v:
@@ -84,7 +89,8 @@ def main(evals):
                     else:
                         f.write(f"_ {k} {' '.join(jobs_info[k])}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = sys.argv[1:]
     to_pass = []
     while args:
